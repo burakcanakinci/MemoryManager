987f2a3ea807b9bbdae9b1fcc80ea8c40e70c890
burakcanakinci <burakakinci.bca@gmail.com>
2024-01-20 14:48:39 +0200

    Header file

--- MemoryManager.h
+++ MemoryManager.h
@@ -1,4 +1,26 @@
-#ifdef MEMORY_MANAGER_H
+#ifndef MEMORY_MANAGER_H
 #define MEMORY_MANAGER_H
 
+#include <iostream>
+#include <vector>
 
+class MemoryManager {
+private:
+    std::vector<bool> memory; // Represents memory blocks (ture for allocated, false for free)
+public:
+    MemoryManager(int size);
+
+    // Allocate memory using First Fit algorithm
+    bool allocateFirstFit(int processId, int size);
+
+    // Allocate memory using Best Fit algorithm
+    bool allocateBestFit(int processId, int size);
+
+    // Deallocatte memory
+    void deallocate(int processId);
+
+    // Display the current state of memory
+    void displayMemory() const;
+};
+
+#endif // MEMORY_MANAGER_H
